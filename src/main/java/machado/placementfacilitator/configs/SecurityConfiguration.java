b228bbba9be753fb3259d3984521233a1ae1d543
Andre Machado <andreluiz.dagamamachado@student.sl.on.ca>
2025-05-13 13:38:46 -0300

    Set CORS allowed origins to allow all origins
    Updated the CORS configuration to accept requests from any origin by replacing a specific allowed origin with a wildcard. This change aims to enable broader access, potentially facilitating development or testing scenarios. Ensure this modification aligns with security requirements before deploying to production.

--- src/main/java/machado/placementfacilitator/configs/SecurityConfiguration.java
+++ src/main/java/machado/placementfacilitator/configs/SecurityConfiguration.java
@@ -56,7 +56,7 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of(
